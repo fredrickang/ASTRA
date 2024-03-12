aa367c6d22590f51e0e909ae10007d8c3a73827d
Woosung Kang <woosungkang@dgist.ac.kr>
2024-03-12 03:55:27 +0000

    sync

--- train_list.sh
+++ train_list.sh
@@ -1,3 +1,1 @@
-python3 ddp_run.py --total_epochs 100 --save_every 5 --batch_size 1024 --save NORMAL --gpus 4 --temp 3.0 --temp_decay 0.956 --lat_constr 2.87 --alpha 1 --loss_type 1 &&
-python3 ddp_run.py --total_epochs 100 --save_every 5 --batch_size 1024 --save AMPLIFIED --gpus 4 --temp 3.0 --temp_decay 0.956 --lat_constr 2.87 --alpha 1 --loss_type 2 &&
-python3 ddp_run.py --total_epochs 100 --save_every 5 --batch_size 1024 --save LATONLY_AMPL --gpus 4 --temp 3.0 --temp_decay 0.956 --lat_constr 2.87 --alpha 1 --loss_type 3 
+python3 ddp_run.py --total_epochs 90 --save_every 5 --batch_size 1024 --save LATCONST50 --gpus 4 --temp 3.0 --temp_decay 0.956 --lat_constr 5.00 --alpha 1 --loss_type 3 
